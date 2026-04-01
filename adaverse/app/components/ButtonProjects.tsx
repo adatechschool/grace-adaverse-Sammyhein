4c086fbf83a813cbf47b80e974b968dff4878c62
Sammyhein <sam.pym@gmail.com>
2026-04-01 10:44:30 +0200

    preparation select

--- adaverse/app/components/ButtonProjects.tsx
+++ adaverse/app/components/ButtonProjects.tsx
@@ -13,19 +13,26 @@ export default function ButtonProject(){
 
             <button onClick={() => dialogRef.current?.close()} className="float-right font-black m-2 p-2 border-2 rounded-full">X</button>
 
-            <article className="p-8 flex flex-col">
+            <form className="relative p-8 flex flex-col">
                 <h1 className="text-(--color-logo) uppercase font-black mt-8 mb-8">Proposer un projet</h1>
 
-                <label htmlFor="title">Titre</label>
-                <input type="text" placeholder="ex: Dashboard ..." className="border border-gray-400 rounded-2xl p-2 mb-4"/>
+                <label htmlFor="title">Titre *</label>
+                <input type="text" name="title" placeholder="ex: Dashboard ..." className="border border-gray-400 rounded-2xl p-2 mb-4" required/>
 
-                <label htmlFor="github">URL GitHub</label>
-                <input type="text" placeholder="https://github.com/..." className="border border-gray-400 rounded-2xl p-2 mb-4"/>
+                <label htmlFor="github">URL GitHub *</label>
+                <input type="text" name="github" placeholder="https://github.com/..." className="border border-gray-400 rounded-2xl p-2 mb-4" required/>
 
-                <label htmlFor="demo">URL de démo</label>
-                <input type="text" placeholder="Titre..." className="border border-gray-400 rounded-2xl p-2 mb-4"/>
+                <label htmlFor="demo">URL de démo *</label>
+                <input type="text" name="demo" placeholder="https://..." className="border border-gray-400 rounded-2xl p-2 mb-4"/>
 
-            </article>
+                <label htmlFor="promo">Promo ADA</label>
+                <select name="promo" id="promo">
+                    
+                </select>
+
+                <p className="absolute font-black right-0 bottom-3">* : Champ Obligatoire</p>
+
+            </form>
         </dialog>
         </>
     )
---
+++ adaverse/app/components/SelectHTML.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { db } from "@/src/data/drizzle"
+import { List } from "@/src/types"
+
+export default function SelectHTML({list} : { list : List}){
+
+    return (
+        <option value=""></option>
+    )
+
+
+}
